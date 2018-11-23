22e87d1bc507f455c48cda4c19da2baf2e41bb8e
jaseemakhtar <jaseemamaljeri@gmail.com>
2018-11-23 12:41:42 +0530

    add program nine

---
+++ networking/programnine/RCEClient.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author jaseem
+ */
+public class RCEClient {
+    public static void main(String[] args){
+        Socket client;
+        InputStream is;
+        OutputStream os;
+        DataOutputStream dos;
+
+        Scanner scan = new Scanner(System.in);
+        String cmd;
+        
+        try{
+            client = new Socket("localhost", 7313);
+            System.out.println("Connected to server: " + client.getInetAddress());
+            System.out.println("Enter the command to execute remotely");
+            cmd = scan.nextLine();
+            
+            is = client.getInputStream();
+            os = client.getOutputStream();
+            
+            dos = new DataOutputStream(os);
+            dos.writeUTF(cmd);
+            
+            int read;
+            byte[] bytes = new byte[1024 * 4];
+            while((read = is.read(bytes)) > 0){
+                String res = new String(bytes);
+                System.out.print(res);
+            }
+            os.flush();
+            client.close();
+            
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ networking/programnine/RCEServer.java
@@ -0,0 +1,53 @@
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author user
+ */
+class RCEServer {
+
+    public static void main(String[] args) {
+           ServerSocket server;
+           Socket client;
+           OutputStream os;
+           InputStream is;
+           InputStream commandInputStream;
+           DataInputStream dis;
+           
+           String[] command;
+           Process process;
+           ProcessBuilder pB = new ProcessBuilder();
+           try{ 
+               server = new ServerSocket(7313);
+               System.out.println("Server started...");
+               client = server.accept();
+               System.out.println("Client connected: " + client.getInetAddress());
+               os = client.getOutputStream();
+               is = client.getInputStream();
+               
+               dis = new DataInputStream(is);
+               
+               String cmd = dis.readUTF();
+               cmd = cmd.trim();
+               command = cmd.split(" ");
+               
+               process = pB.command(command).start();
+               
+               commandInputStream = process.getInputStream();
+               
+               int read;
+               byte[] bytes = new byte[1024 * 4];
+               while((read = commandInputStream.read(bytes)) > 0){
+                   os.write(bytes, 0, read);
+               }
+               System.out.println("Command: " + cmd + " successfully.");
+               System.out.println("Server exited");
+               server.close();
+               
+           }catch(IOException e){
+               System.out.println(e.getMessage());
+           }
+    }
+    
+}
